f332843dd519635fee231f279d37a5a490f53df4
Yrving <yrving.molina@mo2o.com>
2024-09-05 13:01:59 -0500

    feat: init

---
+++ app/src/main/java/com/purple/myapps01/BasicViews.kt
@@ -0,0 +1,15 @@
+package com.purple.myapps01
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BasicViews() {
+
+}
+
+@Preview
+@Composable
+fun BasicViewsPreview() {
+    BasicViews()
+}
---
+++ app/src/main/java/com/purple/myapps01/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.purple.myapps01
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicViews()
+        }
+    }
+
+}
